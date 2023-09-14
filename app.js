dfe8709fb43939fbf74f859622b44902566e8d15
nj1787 <neerajj9@gmail.com>
2023-09-15 01:05:47 +0530

    Posts API implementation initiated.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import userRouter from "./src/features/users/user.routes.js";
+import postRouter from "./src/features/posts/post.routes.js";
 const port = 7100;
 
 const app = express();
 
+// Middlewares - Common To The Application
 app.use(express.json());
+app.use(express.urlencoded());
 
+// Middleware - Routes
 app.use("/api/v1", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 // Test Route
 // app.get("/", (req, res) => {
